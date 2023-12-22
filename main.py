244206d95270d3b7fce676f10b9e3d65c0b20110
Aniket Barik <aniketbarik@outlook.com>
2023-12-22 23:49:02 +0530

    Created a way to get number of lines

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 """Tex-based slot machine.
 """
+MAX_LINES = 3
 
 
 def deposit() -> int:
@@ -9,9 +10,9 @@ def deposit() -> int:
         int: amount deposited by player.
     """
     while True:
-        amount = input("What would you like to deposit?")
-        if amount.isdigit():
-            amount = int(amount)
+        amount_input = input("What would you like to deposit? ")
+        if amount_input.isdigit():
+            amount = int(amount_input)
             if amount > 0:
                 break
             else:
@@ -19,3 +20,34 @@ def deposit() -> int:
         else:
             print("Please enter a number.")
     return amount
+
+
+def get_number_of_lines() -> int:
+    """Get the number of lines to bet on.
+
+    Returns:
+        int: lines to bet on.
+    """
+    while True:
+        lines_input = input(
+            f"""How many lines would you like to bet on (1-{MAX_LINES})? """
+        )
+        if lines_input.isdigit():
+            lines = int(lines_input)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Please enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+    return lines
+
+
+def main() -> None:
+    """main program"""
+    balance: int = deposit()
+    lines: int = get_number_of_lines()
+    print(balance, lines)
+
+
+main()
